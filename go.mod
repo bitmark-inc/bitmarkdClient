380c21d3b6b2fd514b5e113bf33d6eed8665cfa4
Hiếu Phạm <hieupham@bitmark.com>
2025-01-17 16:13:17 +0700

    add Node.BlockDump and Node.BlockDumpRange rpc call

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/lemonlatte/bitmarkdClient
 go 1.16
 
 require (
-	github.com/bitmark-inc/bitmark-sdk-go v0.2.0
 	github.com/bitmark-inc/bitmarkd v0.14.0-beta.1
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.5.1
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- node.go
+++ node.go
@@ -15,3 +15,28 @@ func (bc *BitmarkdRPCClient) GetNodeInfo() (json.RawMessage, error) {
 	err := bc.call("Node.Info", &args, &reply)
 	return reply, err
 }
+
+type BlockDumpRangeArguments struct {
+	Height uint64 `json:"height,string"`
+	Count  int    `json:"count"`
+	Txs    bool   `json:"txs"`
+}
+
+func (bc *BitmarkdRPCClient) BlockDumpRange(height uint64, count int, txs bool) (json.RawMessage, error) {
+	args := BlockDumpRangeArguments{Height: height, Count: count, Txs: txs}
+	var reply json.RawMessage
+	err := bc.call("Node.BlockDumpRange", &args, &reply)
+	return reply, err
+}
+
+type BlockDumpArguments struct {
+	Height uint64 `json:"height,string"`
+	Binary bool   `json:"binary"`
+}
+
+func (bc *BitmarkdRPCClient) BlockDump(height uint64, binary bool) (json.RawMessage, error) {
+	args := BlockDumpArguments{Height: height, Binary: binary}
+	var reply json.RawMessage
+	err := bc.call("Node.BlockDump", &args, &reply)
+	return reply, err
+}
